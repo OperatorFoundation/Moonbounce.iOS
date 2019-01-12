ea2570e6678c92f1219b25036e56d32e46e671fa
Lita <looklita@gmail.com>
2019-01-11 19:00:30 -0600

    Initial Commit

---
+++ Moonbounce.iOSTests/Moonbounce_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Moonbounce_iOSTests.swift
+//  Moonbounce.iOSTests
+//
+//  Created by Adelita Schule on 1/11/19.
+//  Copyright © 2019 Operator Foundation. All rights reserved.
+//
+
+import XCTest
+@testable import Moonbounce_iOS
+
+class Moonbounce_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
